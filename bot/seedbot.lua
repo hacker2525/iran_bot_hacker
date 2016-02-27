b63c768cd15a107cc0483aef8dac50b834eea2c1
hacker2525 <hacker.bot2525@gmail.com>
2016-02-27 22:17:58 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,7 +232,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {117625029,184413821,},--Sudo users
+    sudo_users = {138583448,},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[]],
